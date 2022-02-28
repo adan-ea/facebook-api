2840a0c41fdb85062556aecd01ad67c44ecfa038
adan-ea <57812567+adan-ea@users.noreply.github.com>
2022-02-28 16:20:59 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     .addTag('routes')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/v1', app, document);
 
   await app.listen(3000, 'localhost');
   console.log(`Server running on port ${await app.getUrl()}` + prefix);
